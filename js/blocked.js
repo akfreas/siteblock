0b762588f6fe02f40d8cbcb34516d5e5d20af3b0
Alexander Freas <akfreas@gmail.com>
2016-06-24 15:28:18 +0200

    added remaining time counter on blocked page.

--- js/blocked.js
+++ js/blocked.js
@@ -6,6 +6,17 @@ function onload() {
 
   if (ws !== undefined) {
       var kvs = ws.substring(1).split('&');
+      var state = JSON.parse(localStorage['state']);
+      var next_start = state['ut']['last_end'];
+      var current_time = new Date();
+      var difference = next_start * 1000 - current_time.getTime();
+      var df = new Date(difference);
+
+      var diff_string = df.getHours()+":"+df.getMinutes()+":"+df.getSeconds();
+
+      var remainder = document.getElementById("remaining");
+      remainder.appendChild(document.createTextNode(diff_string));
+    
       for (var i = 0; i < kvs.length; i++) {
          var kv = kvs[i].split('=');
          if (kv[0] == 'url') {
